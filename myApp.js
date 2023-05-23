8a4d48f7475a626f8dccd91488545f2ca84f70da
superwoman <wingjune11@gmail.com>
2023-05-23 14:49:24 +0800

    find people

--- myApp.js
+++ myApp.js
@@ -40,7 +40,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, (err, data) =>{
+    if(err) return console.log(err);
+    done(null, data);
+  })
 };
 
 const findOneByFood = (food, done) => {
