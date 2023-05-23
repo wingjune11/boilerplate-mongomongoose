c5f39b21cdbe7a15259d869640a78b142928bd37
superwoman <wingjune11@gmail.com>
2023-05-23 16:10:18 +0800

    chain

--- myApp.js
+++ myApp.js
@@ -100,8 +100,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({name: 1})
+    .limit(2)
+    .select({age: 0})
+    .exec((err, data) => {
+      if(err) console.log(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
