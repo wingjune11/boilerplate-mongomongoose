14642894fa6f623a6cfee9164bc36c734371de0e
superwoman <wingjune11@gmail.com>
2023-05-23 15:44:10 +0800

    find one and update

--- myApp.js
+++ myApp.js
@@ -77,8 +77,10 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, data) => {
+    if(err) return console.log(err);
+    done(null, data);
+  })
 };
 
 const removeById = (personId, done) => {
